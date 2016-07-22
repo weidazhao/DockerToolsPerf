3d469987183c9d878fec30210e7c51a0a79be5bb
dazhao <dazhao@RED-DAZHAO-H1>
2016-07-21 17:19:25 -0700

    bug fix

--- DockerToolsPerf/src/DockerToolsPerf/DockerTask.ps1
+++ DockerToolsPerf/src/DockerToolsPerf/DockerTask.ps1
@@ -142,7 +142,7 @@ $DockerWorkingDirectory = "/app/"
 $ProjectName = $ProjectName -replace "[^a-zA-Z0-9]", ""
 
 # The name of the image created by the docker file
-$ImageName = "username/dockertoolsperf"
+$ImageName = "weidazhao/dockertoolsperf"
 
 # Calculate the name of the container created by the compose file
 $ContainerName = "${ProjectName}_dockertoolsperf"
